357a5fd9ead09e7d9149e1e98c1b1328e5f2362d
emanuel-xavier <emanueljx@outlook.com>
2024-06-11 17:44:42 -0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"os"
 	"strings"
+)
+
+const (
+	HTTP_VERSION = "HTTP/1.1"
+	CRLF         = "\r\n"
 )
 
 func main() {
@@ -27,53 +33,160 @@ func main() {
 	}
 }
 
+type httpResponse struct {
+	version     string
+	status      int
+	statusMsg   string
+	headers     map[string]string
+	contentType string
+	body        []byte
+}
+
+func newHttpResponse(versin string, status int, statusMsg string, headers map[string]string, body []byte) (res string, err error) {
+	headLine := fmt.Sprintf("%s %d %s%s", versin, status, statusMsg, CRLF)
+	headersStr := ""
+	for k, v := range headers {
+		headersStr += fmt.Sprintf("%s: %s%s", k, v, CRLF)
+	}
+	headersStr += fmt.Sprintf("Content-Length: %d%s%s", len(body), CRLF, CRLF)
+
+	return headLine + headersStr + string(body), nil
+}
+
+func (res httpResponse) parse() string {
+	resStr := fmt.Sprintf("%s %d %s%s", res.version, res.status, res.statusMsg, CRLF)
+
+	for k, v := range res.headers {
+		resStr += fmt.Sprintf("%s:%s%s", k, v, CRLF)
+	}
+	resStr += fmt.Sprintf("Content-Length:%d%s", len(res.body), CRLF)
+	resStr += fmt.Sprintf("Content-Type:%s%s", res.contentType, CRLF)
+	resStr += CRLF
+
+	resStr += string(res.body)
+
+	return resStr
+}
+
 type httpResquest struct {
 	method      string
 	path        string
 	httpVersion string
 	host        string
 	headers     map[string]string
-	body        string
+	body        []byte
 }
 
-func newHttpRequest(rStr string) *httpResquest {
-	requestLine, rStr, found := strings.Cut(rStr, "\r\n")
-	if !found {
-		return nil
+func parseHeadLine(line []byte) (method, path, version string, err error) {
+	partStr := strings.ReplaceAll(string(line), "  ", " ")
+	parts := strings.Split(partStr, " ")
+
+	if len(parts) != 3 {
+		err = fmt.Errorf("invalid request line")
+		return
 	}
 
-	var r httpResquest
+	method = parts[0]
+	path = parts[1]
+	version = parts[2]
 
-	rLine := strings.Split(requestLine, " ")
-	r.method = rLine[0]
-	r.path = rLine[1]
-	r.httpVersion = rLine[2]
+	return
+}
 
-	return &r
+func parserHeader(line []byte) (key, value string, err error) {
+	parts := strings.SplitN(string(line), ":", 2)
+	if len(parts) != 2 {
+		err = fmt.Errorf("invalid header format")
+		return
+	}
+	return parts[0], parts[1], nil
+}
+
+func parseRequest(rBytes []byte) (*httpResquest, error) {
+	lines := bytes.Split(rBytes, []byte(CRLF))
+	method, path, version, err := parseHeadLine(lines[0])
+	if err != nil {
+		return nil, err
+	}
+
+	request := httpResquest{
+		method:      method,
+		path:        path,
+		httpVersion: version,
+		headers:     map[string]string{},
+		body:        make([]byte, 1024),
+	}
+
+	var isParsingHeaders bool
+
+	for _, line := range lines[1:] {
+		switch {
+		case !isParsingHeaders && bytes.Contains(line, []byte(":")):
+			key, value, err := parserHeader(line)
+			if err != nil {
+				return nil, err
+			}
+
+			request.headers[key] = value
+
+		case bytes.Equal(line, []byte(CRLF)):
+			isParsingHeaders = true
+
+		default:
+			request.body = append(request.body, line...)
+			request.body = append(request.body, []byte(CRLF)...)
+		}
+	}
+
+	if len(request.body) > 0 {
+		request.body = request.body[:len(request.body)-len([]byte(CRLF))]
+	}
+
+	return &request, nil
 }
 
 func handleClient(conn net.Conn) {
 	defer conn.Close()
 
 	buffer := make([]byte, 1024)
-	n, err := conn.Read(buffer)
+	_, err := conn.Read(buffer)
 	if err != nil {
 		fmt.Println("Error reading message: ", err.Error())
 	}
 
-	msg := string(buffer[:n])
-	r := newHttpRequest(msg)
-	fmt.Println(r)
+	response := httpResponse{
+		version:     HTTP_VERSION,
+		contentType: "text/plain",
+	}
 
-	var response string
-	if strings.Compare("/", r.path) == 0 {
-		response = "HTTP/1.1 200 OK\r\n\r\n"
+	r, err := parseRequest(buffer)
+	if err != nil {
+		fmt.Println("Erro parsing request: ", err.Error())
+		response = httpResponse{
+			status:    500,
+			statusMsg: "Internal Error",
+		}
 	} else {
-		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+		fmt.Printf("path: |%s| %b\n", (*r).path, strings.Compare("/", (*r).path))
+		for k, v := range (*r).headers {
+			fmt.Printf("%s: %s\n", k, v)
+		}
+		if strings.Compare("/", r.path) == 0 {
+			response.status = 200
+			response.statusMsg = "OK"
+		} else if strings.HasPrefix((*r).path, "/echo/") {
+			bodyContent := strings.Replace((*r).path, "/echo/", "", 1)
+			response.status = 200
+			response.statusMsg = "OK"
+			response.body = []byte(bodyContent)
+		} else {
+			response.status = 404
+			response.statusMsg = "Not Found"
+		}
 	}
 
 	fmt.Println("Sending response:\n", response)
-	_, err = conn.Write([]byte(response))
+	_, err = conn.Write([]byte(response.parse()))
 	if err != nil {
 		fmt.Println("Error sending response: ", err.Error())
 	}
